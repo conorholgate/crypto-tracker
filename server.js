365df59c72eea4a592e55beed5d9522d028d436e
Conor <conor.ianholgate@gmail.com>
2022-05-25 20:18:29 +0100

    update

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-// server.js
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = 3000;
-var hostname = 'localhost';
+// // server.js
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = 3000;
+// var hostname = 'localhost';
 
-app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
- });
+// app.listen(port, hostname, () => {
+//    console.log(`Server running at http://${hostname}:${port}/`);
+//  });
+
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const { createServer } = require('http');
+
+const WebSocket = require('ws');
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/dist')));
+
+const server = createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function (ws) {
+  const id = setInterval(function () {
+    ws.send(JSON.stringify(process.memoryUsage()), function () {
+      //
+      // Ignoring errors.
+      //
+    });
+  }, 100);
+  console.log('started client interval');
+
+  ws.on('close', function () {
+    console.log('stopping client interval');
+    clearInterval(id);
+  });
+});
+
+server.listen(3000, function () {
+  console.log('Listening on http://localhost:8080');
+});
