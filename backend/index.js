fc8d2ec5fef76743f1f702742cf380d7a1973500
Conor Holgate <conor.ianholgate@gmail.com>
2023-03-29 21:45:19 +0100

    updated backend port

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5001
+const port = 8080
 const axios = require('axios')
 
 const headers = {
@@ -13,12 +13,9 @@ app.get('/latest', (req, response) => {
   const { activeSort, sortDir, currency, selectedLimit, start } = req.query
   response.set('Access-Control-Allow-Origin', '*')
   axios
-    .get(
-      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=${start}&convert=${currency}&limit=${selectedLimit}&sort=${activeSort}&sort_dir=${sortDir}`,
-      {
-        headers: headers,
-      }
-    )
+    .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=${start}&convert=${currency}&limit=${selectedLimit}&sort=${activeSort}&sort_dir=${sortDir}`, {
+      headers: headers,
+    })
     .then(res => {
       return response.json(res.data)
     })
@@ -30,12 +27,9 @@ app.get('/search', (req, response) => {
   let search = req.query.search
   response.set('Access-Control-Allow-Origin', '*')
   axios
-    .get(
-      `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=${search}&convert=GBP`,
-      {
-        headers: headers,
-      }
-    )
+    .get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=${search}&convert=GBP`, {
+      headers: headers,
+    })
     .then(res => {
       return response.json(res.data)
     })
