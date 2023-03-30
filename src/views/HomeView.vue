9bc35347516fa0235f1c0238c14fd5c0fed1c93a
Conor Holgate <conor.ianholgate@gmail.com>
2023-03-30 13:19:32 +0100

    refactor to the layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,32 @@
 <template>
   <main>
-    <div class="">
-      <div class="flex flex-col">
-        <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="flex items-center justify-end px-8">
-            <p class="">Show</p>
-            <div class="p-2 ml-2 bg-white rounded-md cursor-pointer w-14 focus:outline-none">
-              <div class="flex" @click="showLimit = !showLimit">
-                {{ cryptoStore.sort.selectedLimit }}
-                <span> <ChevronDownIcon class="w-5 h-5" aria-hidden="true" /></span>
-              </div>
-              <transition
-                enter-active-class="transition duration-200 ease-out"
-                enter-from-class="-translate-y-1 opacity-0"
-                enter-to-class="translate-y-0 opacity-100"
-                leave-active-class="transition duration-150 ease-in"
-                leave-from-class="translate-y-0 opacity-100"
-                leave-to-class="-translate-y-1 opacity-0"
-              >
-                <div v-if="showLimit" class="absolute mr-2 bg-white rounded-b-md w-14 lg:right-8 right-12">
-                  <div v-for="quantity in cryptoStore.availableShowQuantities" :key="quantity">
-                    <p class="px-4 py-1 cursor-pointer hover:bg-gray-200" :class="cryptoStore.sort.selectedLimit === quantity ? 'bg-gray-200' : ''" @click="onSelectLimit(quantity)">{{ quantity }}</p>
-                  </div>
-                </div>
-              </transition>
+    <div class="flex flex-col">
+      <div class="overflow-x-auto">
+        <div class="flex items-center justify-end">
+          <p class="">Show</p>
+          <div class="p-2 ml-2 bg-white rounded-md cursor-pointer w-14 focus:outline-none">
+            <div class="flex" @click="showLimit = !showLimit">
+              {{ cryptoStore.sort.selectedLimit }}
+              <span> <ChevronDownIcon class="w-5 h-5" aria-hidden="true" /></span>
             </div>
+            <transition
+              enter-active-class="transition duration-200 ease-out"
+              enter-from-class="-translate-y-1 opacity-0"
+              enter-to-class="translate-y-0 opacity-100"
+              leave-active-class="transition duration-150 ease-in"
+              leave-from-class="translate-y-0 opacity-100"
+              leave-to-class="-translate-y-1 opacity-0"
+            >
+              <div v-if="showLimit" class="bg-white rounded-b-md w-14">
+                <div v-for="quantity in cryptoStore.availableShowQuantities" :key="quantity">
+                  <p class="px-4 py-1 cursor-pointer hover:bg-gray-200" :class="cryptoStore.sort.selectedLimit === quantity ? 'bg-gray-200' : ''" @click="onSelectLimit(quantity)">{{ quantity }}</p>
+                </div>
+              </div>
+            </transition>
           </div>
-          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <CryptoTable />
-          </div>
+        </div>
+        <div class="inline-block min-w-full py-2 align-middle">
+          <CryptoTable />
         </div>
       </div>
     </div>
