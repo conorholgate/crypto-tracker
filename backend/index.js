1a46a50ee99a84d0d7715ab228c8db6ec1d4e501
Conor Holgate <conor.ianholgate@gmail.com>
2023-04-10 15:42:16 +0100

    added cors middleware

--- backend/index.js
+++ backend/index.js
@@ -6,47 +6,44 @@ const port = 8080
 const axios = require('axios')
 const errorHandler = require('./middleware/errorHandling')
 
-app.use(errorHandler)
+// Setting the cors headers
+const cors = (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  next()
+}
+// Applies the cors headers to all routes
+app.use(cors)
 
+// Setting the headers for all routes
 const headers = {
   'X-CMC_PRO_API_KEY': VITE_API_KEY,
   Accept: 'application/json',
   'Accept-Encoding': 'deflate, gzip',
 }
 
-app.get(
-  '/latest',
-  async (req, response, next) => {
-    const { activeSort, sortDir, currency, selectedLimit, start } = req.query
-    response.set('Access-Control-Allow-Origin', '*')
-    try {
-      let { data } = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=${start}&convert=${currency}&limit=${selectedLimit}&sort=${activeSort}&sort_dir=${sortDir}`, {
-        headers: headers,
-      })
-      return response.json(data)
-    } catch (error) {
-      next(err)
-    }
-  },
-  errorHandler
-)
-app.get(
-  '/metadata',
-  async (req, response, next) => {
-    const { id, slug } = req.query
-    let urlQuery = id ? `id=${id}` : `slug=${slug}`
-    response.set('Access-Control-Allow-Origin', '*')
-    try {
-      let { data } = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?${urlQuery}`, {
-        headers: headers,
-      })
-      return response.json(data)
-    } catch (err) {
-      next(err)
-    }
-  },
-  errorHandler
-)
+app.get('/latest', async (req, response, next) => {
+  const { activeSort, sortDir, currency, selectedLimit, start } = req.query
+  try {
+    let { data } = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=${start}&convert=${currency}&limit=${selectedLimit}&sort=${activeSort}&sort_dir=${sortDir}`, {
+      headers: headers,
+    })
+    return response.json(data)
+  } catch (error) {
+    next(err)
+  }
+})
+app.get('/metadata', async (req, response, next) => {
+  const { id, slug } = req.query
+  let urlQuery = id ? `id=${id}` : `slug=${slug}`
+  try {
+    let { data } = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?${urlQuery}`, {
+      headers: headers,
+    })
+    return response.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
 // app.get('/search', (req, response) => {
 //   let search = req.query.search
 //   response.set('Access-Control-Allow-Origin', '*')
@@ -62,6 +59,9 @@ app.get(
 //     })
 // })
 
+// Applies the error handler to all routes
+
+app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Backend serving on ${port}`)
 })
