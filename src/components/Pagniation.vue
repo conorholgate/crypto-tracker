68ea322ec338de39474cf8f4cee7a9aaaac6b275
Conor Holgate <conor.ianholgate@gmail.com>
2023-04-19 18:40:01 +0100

    refactored pagination code

--- src/components/Pagniation.vue
+++ src/components/Pagniation.vue
@@ -9,7 +9,7 @@
         <div class="flex items-center" v-for="(number, index) in pages" :key="index">
           <div
             class="relative w-12 p-4 mr-1 text-sm font-medium text-center text-gray-500 bg-white border border-gray-300 cursor-pointer hover:bg-gray-50"
-            :class="activePage === pages[index] ? 'bg-indigo-50 border-indigo-500 text-indigo-600' : ''"
+            :class="cryptoStore.activePage === pages[index] ? 'bg-indigo-50 border-indigo-500 text-indigo-600' : ''"
             @click="goToPage(index)"
           >
             {{ number }}
@@ -23,7 +23,7 @@
     </div>
     <div class="flex items-center justify-between md:hidden">
       <button class="flex items-center px-4 py-2.5 bg-white rounded-md" @click="previousPage()"><ChevronLeftIcon class="w-5 h-5" aria-hidden="true" /> <span>Previous</span></button>
-      <span>Page {{ activePage }}</span>
+      <span>Page {{ cryptoStore.activePage }}</span>
       <button class="flex items-center px-4 py-2.5 bg-white rounded-md" @click="nextPage()"><span>Next</span><ChevronRightIcon class="w-5 h-5" aria-hidden="true" /></button>
     </div>
   </div>
@@ -32,39 +32,36 @@
 import { ref } from '@vue/reactivity'
 import { useCryptoStore } from '@/stores/cryptoStore'
 import { SearchIcon, ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/solid'
-import { computed, onMounted } from 'vue'
-import { storeToRefs } from 'pinia'
 
 defineProps({
   coins: Array,
 })
 
 const cryptoStore = useCryptoStore()
-const { activePage } = storeToRefs(cryptoStore)
 let pages = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 const nextPage = () => {
-  activePage.value++
-  if (activePage.value > 10) {
+  cryptoStore.activePage++
+  if (cryptoStore.activePage > 10) {
     pages.value.forEach((page, index) => {
       pages.value[index]++
     })
   }
-  cryptoStore.nextPage(activePage)
+  cryptoStore.nextPage()
 }
 const previousPage = () => {
-  activePage.value > 1 ? activePage.value-- : ''
-  if (activePage.value === pages.value[0]) {
-    if (activePage.value > 1) {
+  cryptoStore.activePage > 1 ? cryptoStore.activePage-- : ''
+  if (cryptoStore.activePage === pages.value[0]) {
+    if (cryptoStore.activePage > 1) {
       pages.value.forEach((page, index) => {
         pages.value[index]--
       })
     }
   }
-  cryptoStore.previousPage(activePage)
+  cryptoStore.previousPage()
 }
 const goToPage = index => {
-  activePage.value = pages.value[index]
-  cryptoStore.goToPage(index, activePage)
+  cryptoStore.activePage = pages.value[index]
+  cryptoStore.goToPage(index)
 }
 </script>
 
